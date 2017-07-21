2a70cc13c24f7147aa1bec9474df4f7b9af7c7a1
Rohan Janjua <rohan.janjua@gmail.com>
2017-07-21 11:18:28 +0100

    fix: don't use babel-preset-es2015 in e2e tests

---
+++ e2eTests/.fructose/index.android.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from "react-native";
+import fructose from "hjkadshhjkl";
+import { loadStories } from './components';
+
+AppRegistry.registerComponent("storybooknative", () => fructose(loadStories));

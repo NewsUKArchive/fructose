857591063af528d5581a1129dc84e77404273155
Rohan Janjua <rohan.janjua@gmail.com>
2017-07-24 17:38:55 +0100

    linting

--- e2eTests/.fructose/index.ios.js
+++ e2eTests/.fructose/index.ios.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from "react-native";
-import fructose from "hjkadshhjkl";
-import { loadStories } from './components';
+import fructose from "@times-components/fructose";
+import { loadStories } from "./components";
 
 AppRegistry.registerComponent("e2eTests", () => fructose(loadStories));

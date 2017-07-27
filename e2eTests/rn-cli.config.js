36e7214dabd8d72a8a0f5a225750a02ac662fd89
Rohan Janjua <rohan.janjua@gmail.com>
2017-07-27 13:12:40 +0100

    chore: blacklist modules from packager that cause @provideModules collisions

--- e2eTests/rn-cli.config.js
+++ e2eTests/rn-cli.config.js
@@ -3,7 +3,9 @@ const blacklist = require("react-native/packager/blacklist");
 module.exports = {
   getBlacklistRE() {
     return blacklist([
-      /\/node_modules\/jest-haste-map\/node_modules\/sane\/node_modules\/fb-watchman\/package.json/
+      /\/node_modules\/jest-haste-map\/node_modules\/.*/,
+      /\/node_modules\/react-native\/node_modules\/fb-watchman\/.*/,
+      /\/node_modules\/@times-components\/fructose\/node_modules\/react-native\/.*/
     ]);
   }
 };

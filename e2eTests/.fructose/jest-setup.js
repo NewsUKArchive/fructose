b908c727d5f4f0363fc957f9bc50f24cc86ee72e
Rohan Janjua <rohan.janjua@gmail.com>
2017-08-01 16:39:24 +0100

    remove detox from fructose

--- e2eTests/.fructose/jest-setup.js
+++ e2eTests/.fructose/jest-setup.js
@@ -1,3 +1,13 @@
 import config from "../package";
 import setup from "@times-components/fructose/setup";
+import detox from "detox";
+
 setup(config.fructose);
+beforeAll( async () => {
+  await detox.init(config.detox);
+}, 180000);
+
+afterAll( async () => {
+  console.log('ended');
+  await detox.cleanup();
+})

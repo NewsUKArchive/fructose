0c965b590657bd30f0418a94fed191a4372c58a7
Rohan Janjua <rohan.janjua@gmail.com>
2017-08-02 17:21:59 +0100

    feature: remove dependency on jest/mocha

--- e2eTests/example/b.fructose.js
+++ e2eTests/example/b.fructose.js
@@ -3,44 +3,51 @@
 import React from "react";
 import { Text } from "react-native";
 
-withComponent(<Text>The Philosopher&apos;s Stone</Text>, "basic text", () => {
+withComponent(<Text>The Philosopher&apos;s Stone</Text>, "basic text", (fructose) => {
   test("simple test", async () => {
+    await fructose.loadComponent();
     await expect(element(by.text(`The Philosopher's Stone`))).toBeVisible();
   });
 });
 
-withComponent(<Text>The Chamber of Secrets</Text>, "basic text", () => {
+withComponent(<Text>The Chamber of Secrets</Text>, "basic text", (fructose) => {
   test("simple test", async () => {
+    await fructose.loadComponent();
     await expect(element(by.text("The Chamber of Secrets"))).toBeVisible();
   });
 });
 
-withComponent(<Text>The Prisoner of Azkaban</Text>, "basic text", () => {
+withComponent(<Text>The Prisoner of Azkaban</Text>, "basic text", (fructose) => {
   test("simple test", async () => {
+    await fructose.loadComponent();
     await expect(element(by.text("The Prisoner of Azkaban"))).toBeVisible();
   });
 });
 
-withComponent(<Text>The Goblet of Fire</Text>, "basic text", () => {
+withComponent(<Text>The Goblet of Fire</Text>, "basic text", (fructose) => {
   test("simple test", async () => {
+    await fructose.loadComponent();
     await expect(element(by.text("The Goblet of Fire"))).toBeVisible();
   });
 });
 
-withComponent(<Text>The Order of the Phoenix</Text>, "basic text", () => {
+withComponent(<Text>The Order of the Phoenix</Text>, "basic text", (fructose) => {
   test("simple test", async () => {
+    await fructose.loadComponent();
     await expect(element(by.text("The Order of the Phoenix"))).toBeVisible();
   });
 });
 
-withComponent(<Text>The Half Blood Prince</Text>, "basic text", () => {
+withComponent(<Text>The Half Blood Prince</Text>, "basic text", (fructose) => {
   test("simple test", async () => {
+    await fructose.loadComponent();
     await expect(element(by.text("The Half Blood Prince"))).toBeVisible();
   });
 });
 
-withComponent(<Text>The Deathly Hallows</Text>, "basic text", () => {
+withComponent(<Text>The Deathly Hallows</Text>, "basic text", (fructose) => {
   test("simple test", async () => {
+    await fructose.loadComponent();
     await expect(element(by.text("The Deathly Hallows"))).toBeVisible();
   });
 });

c23bd17c4073b1eda35b61c89fc69bd853ce037d
Rohan Janjua <rohan.janjua@gmail.com>
2017-08-02 17:21:59 +0100

    feature: remove dependency on jest/mocha

--- e2eTests/.fructose/jest-setup.js
+++ e2eTests/.fructose/jest-setup.js
@@ -5,22 +5,11 @@ import detox from "detox";
 fructose.withComponent();
 
 beforeAll( async () => {
-  console.log(1)
   await fructose.hooks.setup();
-    console.log(2)
-
-
   await detox.init(config.detox);
-    console.log(4)
-
 }, 180000);
 
 afterAll( async () => {
-        await detox.cleanup();
-
-    console.log('ended-1');
+  await detox.cleanup();
   await fructose.hooks.cleanup();
-
-  console.log('ended0');
-    console.log('ended1');
 })

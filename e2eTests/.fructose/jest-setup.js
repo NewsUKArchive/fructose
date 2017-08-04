752b7bd8f85f4f0eef01b3baf3ee682bd35ab637
Rohan Janjua <rohan.janjua@gmail.com>
2017-08-04 16:48:14 +0100

    refactor: lint e2eTests

--- e2eTests/.fructose/jest-setup.js
+++ e2eTests/.fructose/jest-setup.js
@@ -1,34 +1,36 @@
-import config from "../package";
+/* globals beforeAll afterAll*/
 import fructose from "@times-components/fructose/setup";
 import detox from "detox";
-var webdriverio = require('webdriverio');
+import webdriverio from "webdriverio";
 import { spawn } from "child_process";
+import config from "../package";
+
 let appium;
 fructose.withComponent();
 
-beforeAll( async () => {
+beforeAll(async () => {
   await fructose.hooks.setup();
   if (process.env.ANDROID) {
-    appium = await new Promise((resolve) => {
-      const proc = spawn('appium');
+    appium = await new Promise(resolve => {
+      const proc = spawn("appium");
       proc.stdout.on("data", d => {
         if (d.toString("utf8").includes("started on 0.0.0.0:4723")) {
           console.log(d.toString("utf8"));
           resolve(proc);
         }
+      });
     });
-
-    })
-    var options = { 
+    const options = {
       desiredCapabilities: {
-        platformName: 'Android',
-        platformVersion: '7.0',
-        deviceName: 'Android Emulator',
-        app: "/Users/rohanjanjua/workspace/News/fructose/e2eTests/android/app/build/outputs/apk/app-debug.apk"
+        platformName: "Android",
+        platformVersion: "7.0",
+        deviceName: "Android Emulator",
+        app:
+          "/Users/rohanjanjua/workspace/News/fructose/e2eTests/android/app/build/outputs/apk/app-debug.apk"
       },
-      host: 'localhost',
+      host: "localhost",
       port: 4723
-    }
+    };
     global.driver = webdriverio.remote(options);
     await global.driver.init();
     await global.driver.waitForVisible('//*[@text="Fructose"]', 45000);
@@ -37,10 +39,10 @@ beforeAll( async () => {
   }
 }, 180000);
 
-afterAll( async () => {
+afterAll(async () => {
   await detox.cleanup();
   await fructose.hooks.cleanup();
   if (process.env.ANDROID) {
     appium.kill();
   }
-})
+});

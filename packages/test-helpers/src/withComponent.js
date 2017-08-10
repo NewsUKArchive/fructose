9abdfe59ee4d62b875737a19a6131ab633a9a544
Rohan Janjua <rohan.janjua@gmail.com>
2017-08-10 17:52:44 +0100

    fix: removes 'style' from the componentStore keys

--- packages/test-helpers/src/withComponent.js
+++ packages/test-helpers/src/withComponent.js
@@ -1,12 +1,13 @@
 /* globals describe */
 import log from "npmlog";
 import Client from "../../client";
+import rnComponentKey from "../../common/rnComponentKey";
 
 const client = Client(7811);
 log.verbose("client socket", client.socket);
 export default () => {
   const withComponent = (component, description, tests) => {
-    const hashed = JSON.stringify(component);
+    const hashed = rnComponentKey(component);
 
     const loadComponent = async () =>
       client

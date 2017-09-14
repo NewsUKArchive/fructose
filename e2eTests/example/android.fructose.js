3a7c4b0027603c3de2df3b793c723fb5d0a6ba8f
Rohan Janjua <rohan.janjua@gmail.com>
2017-09-14 14:43:21 +0100

    fix: fix e2eTests

--- e2eTests/example/android.fructose.js
+++ e2eTests/example/android.fructose.js
@@ -3,16 +3,20 @@
 import React from "react";
 import { Text } from "react-native";
 
-withComponent(<Text>The Hobbit</Text>, "basic text", fructose => {
-  beforeEach(async () => {
-    await fructose.loadComponent();
-  });
+withComponent(
+  <Text fructoseId="hobbit">The Hobbit</Text>,
+  "basic text",
+  fructose => {
+    beforeEach(async () => {
+      await fructose.loadComponent();
+    });
 
-  test("simple test", async () => {
-    await driver.waitForVisible('//*[@text="The Hobbit"]', 6000);
-    const a = await driver.getText('//*[@text="The Hobbit"]');
-    if (a !== "The Hobbit") {
-      throw new Error("should be the hobbit!");
-    }
-  });
-});
+    test("simple test", async () => {
+      await driver.waitForVisible('//*[@text="The Hobbit"]', 6000);
+      const a = await driver.getText('//*[@text="The Hobbit"]');
+      if (a !== "The Hobbit") {
+        throw new Error("should be the hobbit!");
+      }
+    });
+  }
+);

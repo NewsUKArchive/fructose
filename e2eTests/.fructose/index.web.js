176acfbe615246434bdeb204bb630b6c3e753bf2
Rohan Janjua <rohan.janjua@gmail.com>
2017-09-14 14:39:13 +0100

    BREAKING CHANGE: use a fructoseID instead of JSON.strigify for component loading (#18)
    * chore: adds incorrectly removed yarn.lock file, and removes incorrectly added package-lock.json
    
    * feature: use a specific fructoseID for loading components

--- e2eTests/.fructose/index.web.js
+++ e2eTests/.fructose/index.web.js
@@ -1,4 +1,4 @@
-/* globals document*/
+/* globals document */
 import { AppRegistry } from "react-native";
 import fructose from "@times-components/fructose";
 import { loadStories } from "./components";

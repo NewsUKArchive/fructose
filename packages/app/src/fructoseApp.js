fac9bda55d869fb73c97ce204e4343661ae8e6a8
lowry99 <matthew.lowry@hotmail.co.uk>
2017-09-15 09:23:26 +0100

    fix: disable react warning messages in app

--- packages/app/src/fructoseApp.js
+++ packages/app/src/fructoseApp.js
@@ -6,6 +6,9 @@ const appUI = (components, events) =>
   <FructoseComponent events={events} components={components} />;
 
 export default function Fructose(events) {
+  // to disable the react messing in app which can interfere with tests
+  console.disableYellowBox = true; // eslint-disable-line
+
   return loadComponents => {
     const components = componentLoader(loadComponents);
     return () => appUI(components, events);

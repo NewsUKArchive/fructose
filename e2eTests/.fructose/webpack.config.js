fceec3542d1367b5f3827ea9e99e66f533f65ca1
Rohan Janjua <rohan.janjua@gmail.com>
2017-09-21 18:29:36 +0100

    feat: reduce the amount of config needed to get started with fructose web app

---
+++ e2eTests/.fructose/webpack.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, "./index.web.js")
+  },
+  resolve: {
+    alias: {
+      "react-native": "react-native-web"
+    },
+    extensions: [".web.js", ".js", ".ios.js", ".android.js"],
+    mainFields: ["module", "main"],
+    plugins: [
+      // Use the DLL in development.
+      new webpack.DllReferencePlugin({
+        context: __dirname,
+        manifest: require("../dist/public/vendor-manifest.json")
+      })
+    ]
+  }
+};

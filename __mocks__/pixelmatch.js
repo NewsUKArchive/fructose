485252b6bf395c74e39e5c2fd9faa8e3f5602910
Lowry99 <matthewlowry@hotmail.co.uk>
2017-09-23 09:51:43 +0100

    Chore: fix broken tests

---
+++ __mocks__/pixelmatch.js
@@ -0,0 +1,6 @@
+module.exports = (data1, data2) => {
+  if (data1.equals(data2)) {
+    return 0;
+  }
+  return 1;
+};
---
+++ packages/snapshots/differ.test.js
@@ -0,0 +1,27 @@
+/* globals expect describe it jest */
+import imageDiff from "./differ";
+
+describe("Snapshot differ", () => {
+  jest.mock("pixelmatch");
+
+  const img1 = {
+    data: new Buffer("abc"),
+    width: 5,
+    height: 2
+  };
+  const img2 = {
+    data: new Buffer("abc"),
+    width: 5,
+    height: 2
+  };
+
+  it("returns 0 for equal objects", () => {
+    const { diffCount } = imageDiff(img1, img2);
+    expect(diffCount).toBe(0);
+  });
+
+  it("returns a diff stream", () => {
+    const { diff } = imageDiff(img1, img2);
+    expect(diff.readable).toBe(true);
+  });
+});

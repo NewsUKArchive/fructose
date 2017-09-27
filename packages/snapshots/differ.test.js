51ae1b27abab8419d65a1f7fe006121563d44815
Lowry99 <matthewlowry@hotmail.co.uk>
2017-09-27 10:00:32 +0100

    fix: more decsriptive varibles

--- packages/snapshots/differ.test.js
+++ packages/snapshots/differ.test.js
@@ -21,10 +21,22 @@ describe("Snapshot differ", () => {
 
   it("returns 0 for equal objects", async () => {
     const diff = new PNG({ width: img1.width, height: img1.height });
+    const expectedWidth = 5;
+    const expectedHeight = 2;
+
     await imageDiff(img1, img2);
-    expect(pixelmatch).toBeCalledWith(img1.data, img2.data, diff.data, 5, 2, {
-      threshold: 0.1
-    });
+    expect(
+      pixelmatch
+    ).toBeCalledWith(
+      img1.data,
+      img2.data,
+      diff.data,
+      expectedWidth,
+      expectedHeight,
+      {
+        threshold: 0.1
+      }
+    );
   });
 
   it("returns a diff stream", () => {

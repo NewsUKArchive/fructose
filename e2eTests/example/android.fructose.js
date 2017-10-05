651da0798f5cba3e8579f86f792a42afce4ca3b8
MarkConway94 <markconway1a1@hotmail.com>
2017-10-05 16:22:23 +0100

    added and refactored some android tests

--- e2eTests/example/android.fructose.js
+++ e2eTests/example/android.fructose.js
@@ -1,10 +1,30 @@
 /* globals withComponent test beforeEach driver device afterEach */
 
 import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    fontStyle: "italic"
+  },
+
+  manutd: {
+    fontSize: 72,
+    color: "red",
+    backgroundColor: "black"
+  },
+
+  mayo: {
+    fontSize: 72,
+    color: "red",
+    backgroundColor: "green"
+  }
+});
 
 withComponent(
-  <Text fructoseID="hobbit">The Hobbit</Text>,
+  <Text style={styles.title} fructoseID="hobbit">The Hobbit</Text>,
   "basic text",
   fructose => {
     beforeEach(async () => {
@@ -15,11 +35,47 @@ withComponent(
       await device.reloadReactNative();
     });
 
+    const text = '//*[@text="The Hobbit"]';
+    
     test("simple test", async () => {
-      await driver.waitForVisible('//*[@text="The Hobbit"]', 6000);
-      const a = await driver.getText('//*[@text="The Hobbit"]');
+      await driver.waitForVisible(text, 6000);
+      const a = await driver.getText(text);
       if (a !== "The Hobbit") {
         throw new Error("should be the hobbit!");
+      }
+    });
+  }
+);
+
+withComponent(
+  <Text style={styles.manutd} fructoseID="manutd">
+    Manchester United
+  </Text>,
+  "basic text",
+  fructose => {
+    test("with red style", async () => {
+      await fructose.loadComponent();
+      await driver.waitForVisible('//*[@text="Manchester United"]', 6000);
+      const a = await driver.getText('//*[@text="Manchester United"]');
+      if (a !== "Manchester United") {
+        throw new Error("should be Manchester United!");
+      }
+    });
+  }
+);
+
+withComponent(
+  <Text style={styles.mayo} fructoseID="mayo">
+    Mayo 4 Sam
+  </Text>,
+  "basic text",
+  fructose => {
+    test("with mayo style", async () => {
+      await fructose.loadComponent();
+      await driver.waitForVisible('//*[@text="Mayo 4 Sam"]', 6000);
+      const a = await driver.getText('//*[@text="Mayo 4 Sam"]');
+      if (a !== "Mayo 4 Sam") {
+        throw new Error("should be Mayo 4 Sam!");
       }
     });
   }

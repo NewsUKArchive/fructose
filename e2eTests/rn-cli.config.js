2b011a918975bd3bbc920fae9cb326379f872e33
Rohan Janjua <rohan.janjua@gmail.com>
2017-11-06 18:08:01 +0000

    Revert "hacky hacky"
    This reverts commit 93b829bdb024f919fade5db22364825ea3bcf565.

--- e2eTests/rn-cli.config.js
+++ e2eTests/rn-cli.config.js
@@ -5,8 +5,7 @@ module.exports = {
     return blacklist([
       /\/node_modules\/jest-haste-map\/node_modules\/.*/,
       /\/node_modules\/react-native\/node_modules\/fb-watchman\/.*/,
-      /\/node_modules\/@times-components\/fructose\/node_modules\/react-native\/.*/,
-      /\/node_modules\/@times-components\/fructose\/e2eTests\/node_modules\/react-native\/.*/
+      /\/node_modules\/@times-components\/fructose\/node_modules\/react-native\/.*/
     ]);
   }
 };

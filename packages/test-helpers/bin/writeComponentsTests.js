6dbdadcf91b964a6a234b306f7d4edbb6f639ee6
Rohan Janjua <rohan.janjua@gmail.com>
2017-11-09 16:54:40 +0000

    feature: give users ability to specify which folder and filename to generate tests from

--- packages/test-helpers/bin/writeComponentsTests.js
+++ packages/test-helpers/bin/writeComponentsTests.js
@@ -1,6 +1,44 @@
 #!/usr/bin/env node
-
+const program = require("commander");
 const fs = require("fs");
+
+const version = require("../../../package").version;
+
+program
+  .version(version)
+  .option(
+    "-d, --directory <directory>",
+    "Specify fructose root, default is [.fructose]"
+  )
+  .option(
+    "-t, --test-entry-filename <testEntryFile>",
+    "Specify the test entry file name, default is [components.test.js]"
+  )
+  .option(
+    "-a, --app-entry-filename <appEntryFile>",
+    "Specify the app entry file name, default is [components.js]"
+  )
+  .parse(process.argv);
+
+const directory = program.directory ? program.directory : ".fructose";
+const testEntryFilename = program.testEntryFilename
+  ? program.testEntryFilename
+  : "components.test.js";
+const appEntryFilename = program.appEntryFilename
+  ? program.appEntryFilename
+  : "components.js";
+
+if (!directory) {
+  throw new Error("fructose root not specified, specify with -d");
+}
+
+if (!testEntryFilename) {
+  throw new Error("test entry file not specified, specify with -t");
+}
+
+if (!appEntryFilename) {
+  throw new Error("app entry file is not specified, specify with -a");
+}
 
 const writeFile = (data, path) =>
   new Promise((resolve, reject) => {
@@ -8,21 +46,22 @@ const writeFile = (data, path) =>
       if (err) {
         reject(err);
       }
-      resolve("wrote tests to file");
+      console.log("wrote tests to file");
+      resolve();
     });
   });
 
 const readTestComponents = () =>
   new Promise(resolve => {
     fs.readFile(
-      ".fructose/components.js",
+      `${directory}/${appEntryFilename}`,
       { encoding: "utf8" },
       (err, data) => {
         const tests = data
           .split("\n")
           .filter(line => line.trim().startsWith("require"))
           .join("\n");
-        writeFile(tests, ".fructose/components.test.js").then(resolve);
+        writeFile(tests, `${directory}/${testEntryFilename}`).then(resolve);
       }
     );
   });

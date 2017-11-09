a72905b9244e62be26c115fb90cb17a9b302a860
Rohan Janjua <rohan.janjua@gmail.com>
2017-11-09 22:12:58 +0000

    fix: ensure packager is started in scripts

--- e2eTests/scripts/ios-tests.sh
+++ e2eTests/scripts/ios-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 LOCAL=true npx fructose-tunnel
+react-native start --root .fructose --resetCache --config=rn-cli.config.js &
 react-native run-ios --no-packager
 npm run write-ios-components 
 jest .fructose/components.test.js --verbose --setupTestFrameworkScriptFile ./.fructose/setup.ios.js --forceExit

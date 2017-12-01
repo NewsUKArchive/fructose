cc3f764cccba10ff94558a7d7d0b45b1cec91534
Kouros Aliabadi <kaliabadi@qaworks.com>
2017-12-01 17:38:23 +0000

    fixing e2e tests

--- e2eTests/scripts/web-tests.sh
+++ e2eTests/scripts/web-tests.sh
@@ -1,6 +1,6 @@
 LOCAL=true npx fructose-tunnel
 yarn web:build:vendor-dev
-npm run write-web-components
+npm run write-web-components -d fructose
 npm run web &
 WEB_PID=$!
 jest fructose/components.test.js  --setupTestFrameworkScriptFile ./fructose/setup.web.js --verbose --forceExit

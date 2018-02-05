3001962623758a63748a87566442ec067c6a1d4a
Lowry99 <matthewlowry@hotmail.co.uk>
2018-02-05 15:42:40 +0000

    handling errors in app

---
+++ packages/app/src/components/errorComponent.test.js
@@ -0,0 +1,19 @@
+/* globals describe it beforeEach expect */
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import ErrorComponent from "./errorComponent";
+
+configure({ adapter: new Adapter() });
+
+describe("Error View Handler", () => {
+  let wrapper;
+  let error;
+
+  beforeEach(() => {
+    error = new Error("I am ERRRRRR");
+    wrapper = shallow(<ErrorComponent error={error} />);
+  });
+
+  it("prints out error", () => expect(wrapper).toMatchSnapshot());
+});
--- packages/app/src/fructoseApp.js
+++ packages/app/src/fructoseApp.js
@@ -2,11 +2,11 @@ import React from "react";
 import FructoseComponent from "./components/fructoseComponent";
 import componentLoader from "./componentLoader";
 
-const appUI = (components, events) =>
-  <FructoseComponent events={events} components={components} />;
+const appUI = (components, events) => (
+  <FructoseComponent events={events} components={components} />
+);
 
 export default function Fructose(events) {
-  // disables the yellow console.warn box in the app
   console.disableYellowBox = true; // eslint-disable-line
 
   return loadComponents => {

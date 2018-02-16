1937fcd62c495d21b39307f2b1fb438f9cd17366
Lowry99 <matthewlowry@hotmail.co.uk>
2018-02-16 16:16:37 +0000

    fix: swap didupdate to didmount

--- packages/app/src/components/errorStateComponent.js
+++ packages/app/src/components/errorStateComponent.js
@@ -20,7 +20,7 @@ const styles = StyleSheet.create({
 });
 
 export default class ErrorState extends Component {
-  componentDidUpdate() {
+  componentDidMount() {
     this.props.events.emit("loaded");
   }
 

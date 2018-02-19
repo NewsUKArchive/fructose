efc16ddcd611677ffe91f35c30f95a47eecde124
Miles Illsley <>
2018-02-19 12:17:45 +0000

    chore: refactor navigation component

---
+++ packages/app/src/components/navigation/menuListComponent.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import {
+  View,
+  FlatList,
+  Text,
+  TouchableHighlight,
+  StyleSheet
+} from "react-native";
+import PropTypes from "prop-types";
+
+const styles = StyleSheet.create({
+  menuHeader: {
+    fontSize: 24,
+    margin: 4,
+    marginBottom: 8,
+    color: "black"
+  },
+  menuItem: {
+    margin: 8
+  },
+  menuItemText: {
+    fontSize: 18
+  },
+  menuSeparator: {
+    backgroundColor: "gray",
+    height: StyleSheet.hairlineWidth,
+    width: "100%"
+  }
+});
+
+const menuHeader = headerText => (
+  <Text style={styles.menuHeader}>{headerText}</Text>
+);
+
+const menuSeparator = () => <View style={styles.menuSeparator} />;
+
+const prepareMenuItems = menuArray => menuArray.map(item => ({ key: item }));
+
+export default class MenuList extends Component {
+  constructor(props) {
+    super(props);
+    this.preparedMenuItems = prepareMenuItems(props.menuItems);
+  }
+
+  menuRow(itemId) {
+    return (
+      <TouchableHighlight
+        style={styles.menuItem}
+        onPress={() => this.props.onMenuItemPress(itemId)}
+      >
+        <Text style={styles.menuItemText}>{itemId}</Text>
+      </TouchableHighlight>
+    );
+  }
+
+  render() {
+    return (
+      <FlatList
+        ListHeaderComponent={menuHeader(this.props.menuHeader)}
+        data={this.preparedMenuItems}
+        renderItem={({ item }) => this.menuRow(item.key)}
+        ItemSeparatorComponent={menuSeparator}
+      />
+    );
+  }
+}
+
+MenuList.propTypes = {
+  menuHeader: PropTypes.string.isRequired,
+  menuItems: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onMenuItemPress: PropTypes.func.isRequired
+};

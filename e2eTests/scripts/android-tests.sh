4fff6010b5a4ecb40d9fd6e9892fe614dbbf68e1
Lowry99 <matthewlowry@hotmail.co.uk>
2018-02-19 15:07:40 +0000

    chore: remove appium from android test script

--- e2eTests/scripts/android-tests.sh
+++ e2eTests/scripts/android-tests.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-npx appium &
-APPIUM_PID=$!
 LOCAL=true npx fructose-tunnel
-emulator @fructose_device -no-boot-anim &
+emulator @Nexus_5X_API_25 -no-boot-anim &
 EMU_PID=$!
 adb wait-for-device
 adb reverse tcp:8081 tcp:8081
@@ -14,5 +12,4 @@ BUNDLER_PID=$!
 react-native run-android --no-packager
 jest fructose/components.test.js --verbose --setupTestFrameworkScriptFile ./fructose/setup.native.js --forceExit
 kill -9 $EMU_PID
-kill -9 $APPIUM_PID
 kill -9 $BUNDLER_PID

0bd80e60167b6e0dc705f658c8a4de6edf41b999
L0wry <matthewlowry@hotmail.co.uk>
2018-04-17 18:19:07 +0100

    feat: showcase support

--- packages/app/src/componentLoader.test.js
+++ packages/app/src/componentLoader.test.js
@@ -17,3 +17,46 @@ it("returns an object containing unique keys/values", () => {
   expect(Object.values(loaded)).toContain(obj1);
   expect(Object.values(loaded)).toContain(obj2);
 });
+
+it("returns an object with keys when using .showcasefiles", () => {
+  // mocks the showcase structure that is exported in times-components
+  const mockShowcase = {
+    default: {
+      name: "Primitives/ArticleLabel",
+      children: [
+        {
+          type: "story",
+          name: "Article Label",
+          component: () => "lol"
+        }
+      ]
+    }
+  };
+
+  const getShowcasesObject = () => [mockShowcase];
+
+  const loaded = loader(getShowcasesObject);
+
+  expect(loaded).toEqual({ "Primitives/ArticleLabel/Article Label": "lol" });
+});
+
+it("filters out non story showcases", () => {
+  // mocks the showcase structure that is exported in times-components
+  const mockShowcase = {
+    default: {
+      name: "Primitives/ArticleLabel",
+      children: [
+        {
+          type: "not a story",
+          name: "Article Label",
+          component: () => "lol"
+        }
+      ]
+    }
+  };
+
+  const getShowcasesObject = () => [mockShowcase];
+
+  const loaded = loader(getShowcasesObject);
+  expect(loaded).toEqual({});
+});

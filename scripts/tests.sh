6a8ee9945eca13089baed14989253cb6211b0aa5
Matt Lowry <matthewlowry@hotmail.co.uk>
2018-04-23 11:41:57 +0100

    chore: Update react native (#109)
    * react native upgraded
    
    * babel configured

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -6,17 +6,20 @@ if  yarn test:unit ; then
   echo "UNIT TESTS PASSED"
 else
   echo "UNIT TESTS FAILED"
-  cd ../..
   exit 1
 fi
 
-cd e2eTests 
-rm -rf node_modules
-yarn
-if  yarn test ; then
-  echo "E2E TESTS PASSED"
+if  yarn e2e:test:web ; then
+  echo "WEB TESTS PASSED"
 else
   echo "E2E TESTS FAILED"
-  cd ../..
   exit 1
 fi
+
+if  yarn e2e:test:ios ; then
+  echo "IOS TESTS PASSED"
+else
+  echo "E2E TESTS FAILED"
+  exit 1
+fi
+

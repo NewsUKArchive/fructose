af9df6731ddf082f08128b8077849b8777bac3c5
L0wry <matthewlowry@hotmail.co.uk>
2018-04-30 18:09:58 +0100

    BREAKING CHANGE: adding filtering based on platform

--- packages/app/src/componentLoader.test.js
+++ packages/app/src/componentLoader.test.js
@@ -45,3 +45,46 @@ it("filters out non story showcases", () => {
   const loaded = loader(getShowcasesObject);
   expect(loaded).toEqual({});
 });
+
+it("returns showcases without a platform", () => {
+  const mockShowcase = {
+    default: {
+      name: "Primitives/ArticleLabel",
+      children: [
+        {
+          type: "story",
+          name: "Article Label",
+          component: () => "lol"
+        }
+      ]
+    }
+  };
+
+  const getShowcasesObject = () => [mockShowcase];
+  const loadedShowcases = loader(getShowcasesObject);
+  const returnedComponent = loadedShowcases[
+    "Primitives/ArticleLabel/Article Label"
+  ]();
+  expect(returnedComponent).toEqual("lol");
+});
+
+it("filters out showcases that don't match specified plaform", () => {
+  const mockShowcase = {
+    default: {
+      name: "Primitives/ArticleLabel",
+      children: [
+        {
+          platform: "native",
+          type: "story",
+          name: "Article Label",
+          component: () => "lol"
+        }
+      ]
+    }
+  };
+
+  const getShowcasesObject = () => [mockShowcase];
+
+  const loaded = loader(getShowcasesObject, "web");
+  expect(loaded).toEqual({});
+});
--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -4,8 +4,9 @@ import App from "./components/app";
 import Messaging from "./messaging";
 import componentLoader from "./componentLoader";
 
-export default componentsToLoad => () => {
-  const components = componentLoader(componentsToLoad);
+export default (componentsToLoad, config) => () => {
+  const platform = config.platform || "native";
+  const components = componentLoader(componentsToLoad, platform);
   const componentList = Object.keys(components).map(key => key);
 
   return (

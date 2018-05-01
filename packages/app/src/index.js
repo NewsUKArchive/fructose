1f252d347b215b65780be8b36fb912b7e256989c
L0wry <matthewlowry@hotmail.co.uk>
2018-05-01 11:01:08 +0100

    fix: default platform

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -4,8 +4,10 @@ import App from "./components/app";
 import Messaging from "./messaging";
 import componentLoader from "./componentLoader";
 
+const defaultPlatform = "native";
+
 export default (componentsToLoad, config) => () => {
-  const platform = config.platform || "native";
+  const platform = config.platform || defaultPlatform;
   const components = componentLoader(componentsToLoad, platform);
   const componentList = Object.keys(components).map(key => key);
 

67847c524c81bdcda079471d2085ff9752ab6666
L0wry <matthewlowry@hotmail.co.uk>
2018-05-01 10:57:37 +0100

    fix: setting index config

--- e2eTests/fructose/index.web.js
+++ e2eTests/fructose/index.web.js
@@ -3,7 +3,9 @@ import { AppRegistry } from "react-native";
 import fructose from "../../packages/app/src";
 import { getStories } from "./components"; // eslint-disable-line import/no-unresolved
 
-AppRegistry.registerComponent("e2eTests", () => fructose(getStories));
+AppRegistry.registerComponent("e2eTests", () =>
+  fructose(getStories, { platform: "web" })
+);
 AppRegistry.runApplication("e2eTests", {
   rootTag: document.getElementById("react-root")
 });

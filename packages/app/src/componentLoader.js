c71fc5af3e24620701ece88ad6dd47e37e54dd28
L0wry <matthewlowry@hotmail.co.uk>
2018-05-04 14:49:47 +0100

    fix: removing / from names

--- packages/app/src/componentLoader.js
+++ packages/app/src/componentLoader.js
@@ -26,12 +26,13 @@ export default (loadComponents, platform) => {
         );
 
         filteredShowcases.forEach(showcase => {
-          const showCaseName = `${showcases.name}/${showcase.name}`;
+          const nameWithoutSlash = showcases.name.split("/")[0];
+          const showCaseName = `${nameWithoutSlash}/${showcase.name}`;
           componentsStore[showCaseName] = showcase.component;
         });
       }
     });
   }
-
+  console.log(componentsStore);
   return componentsStore;
 };

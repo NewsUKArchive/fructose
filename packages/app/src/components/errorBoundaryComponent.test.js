751b4a19900db09aa4efb52c5d1943dee3961b1a
L0wry <matthewlowry@hotmail.co.uk>
2018-05-15 16:31:23 +0100

    test: for error view

---
+++ packages/app/src/components/errorBoundaryComponent.test.js
@@ -0,0 +1,36 @@
+/* globals it expect */
+
+import React from "react";
+import { Text } from "react-native";
+import { EventEmitter } from "events";
+import renderer from "react-test-renderer";
+
+import ErrorBoundary from "./errorBoundaryComponent";
+
+const ThrowAnError = () => <Text>{lol}</Text>; // eslint-disable-line
+
+describe("Error Boundary", () => {
+  it("It renders the error View when error is triggered", () => {
+    const wrapper = renderer
+      .create(
+        <ErrorBoundary events={new EventEmitter()}>
+          <ThrowAnError />
+        </ErrorBoundary>
+      )
+      .toJSON();
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it("it render a component when there is no error", () => {
+    const wrapper = renderer
+      .create(
+        <ErrorBoundary events={new EventEmitter()}>
+          <Text>Hey I rendered</Text>
+        </ErrorBoundary>
+      )
+      .toJSON();
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});

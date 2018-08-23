ec976f8970d2972cfa9c52c20d0c650de0b7ae36
Matt Lowry <matthewlowry@hotmail.co.uk>
2018-08-23 14:00:34 +0100

    fix: cleaning up test session (#144)

--- e2eTests/scripts/ios-tests.sh
+++ e2eTests/scripts/ios-tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 xcrun simctl boot 'iPhone 7'
+export IPHONE=$!
 ./node_modules/.bin/rnscl --searchDir ./e2eTests/ --pattern 'example/component.showcase.js' --outputFile e2eTests/fructose/components.js
 ./node_modules/.bin/react-native start --resetCache &
+export PACKAGER=$!
 ./node_modules/.bin/react-native run-ios --no-packager
 LOGLEVEL=verbose node_modules/.bin/jest ./e2eTests/example/ios.test.js --verbose --forceExit
+
+kill $IPHONE
+kill $PACKAGER

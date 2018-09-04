1aa40a5be703d5a66bd3cb3695fafbddab6027dd
L0wry <matthewlowry@hotmail.co.uk>
2018-09-04 14:03:18 +0100

    fix: comments

--- e2eTests/fructose/index.web.js
+++ e2eTests/fructose/index.web.js
@@ -1,8 +1,8 @@
 /* globals document */
 
+import { AppRegistry, Text } from "react-native";
 import fructose from "../../packages/app/src";
 import { getStories } from "./components"; // eslint-disable-line import/no-unresolved
-import { AppRegistry, Text } from "react-native";
 
 AppRegistry.registerComponent("e2eTests", () =>
   fructose(getStories, { platform: "web" })

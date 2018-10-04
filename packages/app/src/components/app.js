ed26a0580af94cae700a35bcaf05339d408f208a
L0wry <matthewlowry@hotmail.co.uk>
2018-10-04 16:37:42 +0100

    refactor: stateless app.js

--- packages/app/src/components/app.js
+++ packages/app/src/components/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
   createDrawerNavigator
@@ -6,19 +6,12 @@ import {
 import getNavigationScreens from './getNavigationScreens';
 import Navigation from "./navigation/navigation"
 
-const rootStack = (components) => createDrawerNavigator(getNavigationScreens(components), {
+const App = ({ components }) => {
+  const Root = createDrawerNavigator(getNavigationScreens(components), {
     contentComponent: Navigation
   });
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const RootStack = rootStack(this.props.components)
-    return <RootStack />;
-  }
+  return <Root/>
 }
 
 App.propTypes = {

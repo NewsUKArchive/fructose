e3f1ea91880f03f9c6c24f223dba48abfd2369c8
L0wry <matthewlowry@hotmail.co.uk>
2018-10-09 14:55:58 +0100

    refactor: loading screen

--- packages/app/src/components/getNavigationScreens.js
+++ packages/app/src/components/getNavigationScreens.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ErrorBoundary from './errorBoundaryComponent';
-import FructoseComponentWrapper from './fructoseComponentWrapper';
-import LoadingScreen from "./loadingScreen"
+import React from "react";
+import ErrorBoundary from "./errorBoundaryComponent";
+import FructoseComponentWrapper from "./fructoseComponentWrapper";
+import LoadingScreen from "./loadingScreen";
 
-export default (componentsToLoad) => {
+export default componentsToLoad => {
   const navigationList = {
-      Home: {
-        screen: LoadingScreen
-      }
-    };
+    Home: {
+      screen: <LoadingScreen />
+    }
+  };
 
-    Object.keys(componentsToLoad).forEach(component => {
-      navigationList[component] = {
-          screen: () => (
-            <ErrorBoundary>
-              <FructoseComponentWrapper component={componentsToLoad[component]} />
-            </ErrorBoundary>
-          )
-        }
-      })
-  
-  return navigationList
-}
+  Object.keys(componentsToLoad).forEach(component => {
+    navigationList[component] = {
+      screen: () => (
+        <ErrorBoundary>
+          <FructoseComponentWrapper component={componentsToLoad[component]} />
+        </ErrorBoundary>
+      )
+    };
+  });
+
+  return navigationList;
+};
